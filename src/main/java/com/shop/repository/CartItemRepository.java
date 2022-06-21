713a15fd6ad9747882b62e1d6ffaaa149b71c5a1
alswhd20 <alswhd20@gmail.com>
2022-06-21 12:02:42 +0900

    장바구니 구현

--- src/main/java/com/shop/repository/CartItemRepository.java
+++ src/main/java/com/shop/repository/CartItemRepository.java
@@ -3,10 +3,13 @@ package com.shop.repository;
 
 import com.shop.dto.CartDetailDto;
 import com.shop.entity.CartItem;
+import com.shop.entity.Order;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -23,7 +26,6 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
             "and im.repimgYn = 'Y' " +
             "order by ci.regTime desc"
     )
-    List<CartDetailDto> findCartDetailDtoList(Long cartId);
-
+    List<CartDetailDto> findCartDetailDtoList(@Param("cartId") Long cartId);
 
 }
